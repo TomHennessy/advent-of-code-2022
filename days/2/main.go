9a380595fdfc6aa0b2f50c5a4e36a4e113996ab6
Tom Hennessy <tmhennessy@outlook.com>
2023-05-23 23:05:27 +0100

    Day 2 part 1 much cleaner

--- days/2/main.go
+++ days/2/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"log"
 	"os"
-	"strings"
 )
 
 const scoreForLoss = 0
@@ -15,49 +14,26 @@ const shapeRock = 1
 const shapePaper = 2
 const shapeScissors = 3
 
-func getOutcome(elfHand, myResponse string) int {
+func main() {
 
-	var score int
-
-	if elfHand == "A" {
-		if myResponse == "X" {
-			// tie
-			score = scoreForTie + shapeRock
-		} else if myResponse == "Y" {
-			// win
-			score = scoreForWin + shapePaper
-		} else if myResponse == "Z" {
-			// loss
-			score = scoreForLoss + shapeScissors
-		}
-	} else if elfHand == "B" {
-		if myResponse == "X" {
-			// loss
-			score = scoreForLoss + shapeRock
-		} else if myResponse == "Y" {
-			// tie
-			score = scoreForTie + shapePaper
-		} else if myResponse == "Z" {
-			// win
-			score = scoreForWin + shapeScissors
-		}
-	} else if elfHand == "C" {
-		if myResponse == "X" {
-			// win
-			score = scoreForWin + shapeRock
-		} else if myResponse == "Y" {
-			// loss
-			score = scoreForLoss + shapePaper
-		} else if myResponse == "Z" {
-			// tie
-			score = scoreForTie + shapeScissors
-		}
+	scoreSheet := map[byte]map[byte]int{
+		'A': {
+			'X': scoreForTie + shapeRock,      // elf: rock, me: rock
+			'Y': scoreForWin + shapePaper,     // elf: rock, me: paper
+			'Z': scoreForLoss + shapeScissors, // elf: rock, me: scissors
+		},
+		'B': {
+			'X': scoreForLoss + shapeRock,    // elf: paper, me: rock
+			'Y': scoreForTie + shapePaper,    // elf: paper, me: paper
+			'Z': scoreForWin + shapeScissors, // elf: paper, me: scissors
+		},
+		'C': {
+			'X': scoreForWin + shapeRock,     // elf: scissors, me: rock
+			'Y': scoreForLoss + shapePaper,   // elf: scissors, me: paper
+			'Z': scoreForTie + shapeScissors, // elf: scissors, me: scissors
+		},
 	}
 
-	return score
-}
-
-func main() {
 	file, err := os.Open("./input.txt")
 
 	if err != nil {
@@ -72,38 +48,11 @@ func main() {
 	for scanner.Scan() {
 
 		thisLine := scanner.Text()
-		// thisLine looks like this:
-		// A X
 
-		elfHand := strings.Split(thisLine, " ")[0]
-		myResponse := strings.Split(thisLine, " ")[1]
+		elfHand := thisLine[0]
+		myResponse := thisLine[2]
 
-		totalScore += getOutcome(elfHand, myResponse)
-
-		// var current int
-		// fmt.Sscanf(scanner.Text(), "%d", &current)
-
-		// col 1 is the hand of the elf
-		// A = rock
-		// B = paper
-		// C = scissors
-
-		// col 2 is my response
-		// X = rock
-		// Y = paper
-		// Z = scissors
-
-		// scores:
-
-		// shape:
-		// 1 for rock
-		// 2 for paper
-		// 3 for scissors
-
-		// outcomes:
-		// 0 for loss
-		// 3 for tie
-		// 6 for win
+		totalScore += scoreSheet[elfHand][myResponse]
 
 	}
 
