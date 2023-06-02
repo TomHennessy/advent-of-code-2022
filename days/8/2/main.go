b6fcdd7f3f7a41224ee246b200e6e98d9873377f
Tom Hennessy <tmhennessy@outlook.com>
2023-06-02 15:13:00 +0100

    Day 7 part two done

--- days/8/2/main.go
+++ days/8/2/main.go
@@ -29,9 +29,7 @@ func main() {
 	treeArrays := make(map[string]map[int]map[int]int, 0)
 
 	treeArrays["left"] = make(map[int]map[int]int, 0)
-	treeArrays["right"] = make(map[int]map[int]int, 0)
 	treeArrays["top"] = make(map[int]map[int]int, 0)
-	treeArrays["bottom"] = make(map[int]map[int]int, 0)
 
 	rowIndex := 0
 
@@ -46,13 +44,11 @@ func main() {
 		arraysLength = len(thisLine)
 
 		treeArrays["left"][rowIndex] = make(map[int]int, arraysLength)
-		treeArrays["right"][rowIndex] = make(map[int]int, arraysLength)
 
 		for i, char := range strings.Split(thisLine, "") {
 
 			if rowIndex == 0 {
 				treeArrays["top"][i] = make(map[int]int, arraysLength)
-				treeArrays["bottom"][i] = make(map[int]int, arraysLength)
 			}
 
 			charAsInt, err := strconv.Atoi(char)
@@ -60,10 +56,8 @@ func main() {
 			check(err)
 
 			treeArrays["left"][rowIndex][i] = charAsInt
-			treeArrays["right"][rowIndex][arraysLength-i-1] = charAsInt
 
 			treeArrays["top"][i][rowIndex] = charAsInt
-			treeArrays["bottom"][i][arraysLength-rowIndex-1] = charAsInt
 
 		}
 
@@ -71,42 +65,57 @@ func main() {
 
 	}
 
-	// we now have the left and right, top and bottom arrays for each row
-	// I then check each index against each of the other lists to see if it can be seen from any direction
+	// now we want to check each index (x, y) and see what the four distances are that it can see
 
-	treesThatCanBeSeen := make(map[string]map[int]map[int]int, 0)
-
-	treesThatCanBeSeen["left"] = make(map[int]map[int]int, 0)
-	treesThatCanBeSeen["right"] = make(map[int]map[int]int, 0)
-	treesThatCanBeSeen["top"] = make(map[int]map[int]int, 0)
-	treesThatCanBeSeen["bottom"] = make(map[int]map[int]int, 0)
-
-	for i := 0; i < arraysLength; i++ {
-
-		treesThatCanBeSeen["left"][i] = canBeSeen(treeArrays["left"][i])
-		treesThatCanBeSeen["right"][i] = canBeSeen(treeArrays["right"][i])
-		treesThatCanBeSeen["top"][i] = canBeSeen(treeArrays["top"][i])
-		treesThatCanBeSeen["bottom"][i] = canBeSeen(treeArrays["bottom"][i])
-
-	}
-
-	// then check the lists against each other
-	totalCanBeSeen := 0
+	highestDistance := 0
 
 	for y := 0; y < arraysLength; y++ {
 
 		for x := 0; x < arraysLength; x++ {
 
-			reverseX := arraysLength - x - 1
-			reverseY := arraysLength - y - 1
+			distances := make(map[string]int, 0)
 
-			if treesThatCanBeSeen["left"][y][x] == 1 ||
-				treesThatCanBeSeen["right"][y][reverseX] == 1 ||
+			distances["left"] = 0
+			distances["right"] = 0
+			distances["top"] = 0
+			distances["bottom"] = 0
 
-				treesThatCanBeSeen["top"][x][y] == 1 ||
-				treesThatCanBeSeen["bottom"][x][reverseY] == 1 {
+			// left
+			for i := x - 1; i >= 0; i-- {
+				distances["left"]++
+				if treeArrays["left"][y][i] >= treeArrays["left"][y][x] {
+					break
+				}
+			}
 
-				totalCanBeSeen++
+			// right
+			for i := x + 1; i < arraysLength; i++ {
+				distances["right"]++
+				if treeArrays["left"][y][i] >= treeArrays["left"][y][x] {
+					break
+				}
+			}
+
+			// top
+			for i := y - 1; i >= 0; i-- {
+				distances["top"]++
+				if treeArrays["top"][x][i] >= treeArrays["top"][x][y] {
+					break
+				}
+			}
+
+			// bottom
+			for i := y + 1; i < arraysLength; i++ {
+				distances["bottom"]++
+				if treeArrays["top"][x][i] >= treeArrays["top"][x][y] {
+					break
+				}
+			}
+
+			thisTotalDistance := distances["left"] * distances["right"] * distances["top"] * distances["bottom"]
+
+			if thisTotalDistance > highestDistance {
+				highestDistance = thisTotalDistance
 
 			}
 
@@ -114,33 +123,7 @@ func main() {
 
 	}
 
-	fmt.Println(totalCanBeSeen)
-
-}
-
-func canBeSeen(array map[int]int) map[int]int {
-
-	// this cuts down any trees that can't be seen, marking them as 1
-
-	currentHighest := -1
-	canBeSeenList := make(map[int]int, len(array))
-
-	for index := 0; index < len(array); index++ {
-
-		height := array[index]
-
-		if height > currentHighest {
-			currentHighest = height
-
-			canBeSeenList[index] = 1
-
-		} else {
-			canBeSeenList[index] = 0
-		}
-
-	}
-
-	return canBeSeenList
+	fmt.Println(highestDistance)
 }
 
 func timer(name string) func() {
